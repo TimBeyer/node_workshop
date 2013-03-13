d91b41ca6e02d6e8e14b69808cec376fb294621f
Tim Beyer <tim.beyer@deliveryhero.com>
2013-03-13 21:43:56 +0100

    Add awesome static page

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("Hello");
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+app.use(express.static(__dirname + '/static'));
+
+app.listen(PORT);
+
+console.log("Server now listening on port", PORT);
