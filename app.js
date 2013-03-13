60f57d90377626899e0511e242201d537a4522e5
Tim Beyer <tim.beyer@deliveryhero.com>
2013-03-13 22:09:52 +0100

    Add unimplemented API

--- app.js
+++ app.js
@@ -4,6 +4,32 @@ var PORT = 3000;
 
 app.use(express.static(__dirname + '/static'));
 
+app.get('/notes', function (req, res){
+    // respond with collection of notes
+    console.log('/notes');
+    res.json([
+        { a: 'list' },
+        { of: 'notes'}
+    ]);
+});
+
+app.post('/notes', function (req, res) {
+    // Create a new note and respond with it
+});
+
+app.get('/notes/:id', function (req, res) {
+    // respond with a specific node
+});
+
+app.put('/notes/:id', function (req, res) {
+    // update a note with an id and respond with it
+});
+
+app.delete('/notes/:id', function (req, res) {
+    // delete a note with an ID and respond with it
+});
+
+
 app.listen(PORT);
 
 console.log("Server now listening on port", PORT);
